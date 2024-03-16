4502e3ebf578f83fe7057a68363bdf4bd0030540
Nikolai <nikoliktodorov@gmail.com>
2024-03-16 23:07:58 +0200

    smal fix <3

--- frontend/src/pages/Game/Deck/Deck.jsx
+++ frontend/src/pages/Game/Deck/Deck.jsx
@@ -3,7 +3,12 @@ import Card from "../Card/Card"
 
 export default function Deck(){
 
-    const [cards,setCards] = useState([{image:"https://i.imgur.com/8nLFCVP.png"}])
+    const [cards,setCards] = useState([
+        {image:"https://i.imgur.com/8nLFCVP.png"},
+        {image:"https://i.imgur.com/8nLFCVP.png"},
+        {image:"https://i.imgur.com/8nLFCVP.png"},
+        {image:"https://i.imgur.com/8nLFCVP.png"}
+    ])//For Test
 
     return(
         <>
--- frontend/src/pages/Session/Session.jsx
+++ frontend/src/pages/Session/Session.jsx
@@ -1,13 +1,12 @@
 import { useParams } from "react-router-dom";
 import styles from "./Session.module.css"
 import { useEffect } from "react";
-import Chat from "./Chat/Chat"
-import styles from "./Game.module.css"
-import TopLabel from "./TopLabel/TopLabel"
-import Deck from "./Deck/Deck"
+import Chat from "../Game/Chat/Chat";
 
-export default function Session ({children, name, ...props}) {
-    const name = useParams();
+import Deck from "../Game/Deck/Deck"
+
+export default function Session ({children, ...props}) {
+    const  {name} = useParams();
     useEffect(() => {
         console.log(name)
     }, [])
