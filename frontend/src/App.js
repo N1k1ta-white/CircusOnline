3519ff83eaa8c91977f1d10cdc7c2993c4096f89
Roman Lytvynov <litvinromeo@gmail.com>
2024-03-16 12:49:05 +0200

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Game from "./pages/Game/Game";
 import Home from './pages/Home/Home';
-import Authorization from './pages/Authorization/Authorization';
 import Header from "./components/Header/Header";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProfile } from './utils/redux/authActions';
 
 function App() {
+    const dispatch = useDispatch()
+    const { userInfo } = useSelector(state => state.auth)
+    useEffect(() => {
+        const updateToken = async () => {
+        const data = await dispatch(fetchProfile())
+        console.log(data)
+        if (Object.keys(userInfo).length !== 0 && 'accessToken' in userInfo) {
+          window.localStorage.setItem('accessToken', data.payload.accessToken)
+        } 
+    }
+    updateToken()
+    // eslint-disable-next-line
+  }, [dispatch])
   return (
       <>
           <Header/>
@@ -13,7 +27,6 @@ function App() {
               <Routes>
                     <Route path='/' element={<Home/>}/>
                     <Route path='/game' element={<Game/>}/>
-                    <Route path='/auth' element={<Authorization/>}/>
               </Routes>
           </BrowserRouter>
       </>
