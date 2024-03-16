1fd6b731b51b87cbf3a49b6173434cecc799aa06
Roman Lytvynov <litvinromeo@gmail.com>
2024-03-16 10:43:51 +0200

    based settings v2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Game from "./pages/Game/Game";
+import Home from './pages/Home/Home';
+import Authorization from './pages/Authorization/Authorization';
 import Header from "./components/Header/Header";
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
           <Header/>
           <BrowserRouter>
               <Routes>
-                  <Route path='/game' element={<Game/>}/>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/game' element={<Game/>}/>
+                    <Route path='/auth' element={<Authorization/>}/>
               </Routes>
           </BrowserRouter>
       </>
